de2c3f46e1bab140ad66e4669d98653c5df6d446
DIVYASRI <120447261+tdivyasri@users.noreply.github.com>
2024-11-23 22:38:57 +0530

    Add files via upload

---
+++ load_steps.py
@@ -0,0 +1,11 @@
+# Archivo: load_steps.py
+
+from behave import given, when, then
+# Importar cualquier cosa necesaria para cargar los datos BDD
+
+@given('I have some initial products')
+def initial_products(context):
+    # Carga de productos iniciales para las pruebas
+    pass
+
+# Otras definiciones de pasos para cargar datos BDD según tu contexto
